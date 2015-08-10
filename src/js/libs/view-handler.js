79da1f909bc179060941f304cac0111ae8019e64
Jani Nieminen <jpkniem@gmail.com>
2015-08-10 14:01:58 +0300

    Extend backbone to have transition effects.

--- src/js/libs/view-handler.js
+++ src/js/libs/view-handler.js
@@ -1,75 +1,75 @@
 define(function(require) {
  "use strict";
 
-        var $ = require('jquery');
-        var Backbone = require('backbone');
+  var $ = require('jquery');
+  var Backbone = require('backbone');
 
-        var stateEvents = require('libs/state-events');
+  var stateEvents = require('libs/state-events');
 
-        var $body = $("body");
-        var $content = $(".content");
-        var $nav = $("#appnav");
+  var $body = $("body");
+  var $content = $(".content");
+  var $nav = $("#appnav");
 
-        var currentView;
+  var currentView;
 
-        // Adds Close to Backbone Views
-        Backbone.View.prototype.close = function(){
-            this.remove();
-            this.unbind();
-            if (this.onClose)
-              this.onClose();
-      };
+      // Adds Close to Backbone Views
+  Backbone.View.prototype.close = function(){
+    this.remove();
+    this.unbind();
+    if (this.onClose)
+        this.onClose();
+  };
 
-    Backbone.Transitions = {};
-    Backbone.Transitions.transit = function(view, transitionType) {
-        var _self = this;
+  Backbone.Transitions = {};
+  Backbone.Transitions.transit = function(view, transitionType) {
+      var _self = this;
 
-        transitionType = transitionType || "slideInOut";
-         _self.transitionType = transitionType;
+      transitionType = transitionType || "slideInOut";
+       _self.transitionType = transitionType;
 
-        if (_self.currView) {
-            _self.currView.$(".page")
-            .addClass(_self.transitionType)
-            .removeClass("active");
-            //Render the new view after the transition has finished on the current view
-            _self.currView.$(".page").one("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
-            _self.currView.$(".page").remove();
-            _self.render(view);
-            });
-        } else {
-            _self.render(view);
-        }
-        _self.currView = view;
+      if (_self.currView) {
+          _self.currView.$(".page")
+          .addClass(_self.transitionType)
+          .removeClass("active");
+          //Render the new view after the transition has finished on the current view
+          _self.currView.$(".page").one("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
+          _self.currView.$(".page").remove();
+          _self.render(view);
+          });
+      } else {
+          _self.render(view);
+      }
+      _self.currView = view;
 
-    };
-    Backbone.Transitions.render = function(view) {
-        var _self = this;
+  };
+  Backbone.Transitions.render = function(view) {
+      var _self = this;
 
-        // Call the render on view object
-        view.render();
-        view.$(".page").addClass(_self.transitionType);
+      // Call the render on view object
+      view.render();
+      view.$(".page").addClass(_self.transitionType);
 
-        // Delay the transitions on newly added elements by 20 s
-        // to prevent the flickering effect
-        _.delay(function() {
-            view.$(".page")
-                .addClass(_self.transitionType)
-                .addClass("active");
-        }, 20);
-    };
+      // Delay the transitions on newly added elements by 20 s
+      // to prevent the flickering effect
+      _.delay(function() {
+          view.$(".page")
+              .addClass(_self.transitionType)
+              .addClass("active");
+      }, 20);
+  };
 
-    var ViewHandler = {
-        setCurrent: function(view, title) {
-            if(title)
-                stateEvents.trigger("update:title", title);
+  var ViewHandler = {
+      setCurrent: function(view, title) {
+          if(title)
+              stateEvents.trigger("update:title", title);
 /*
-            if(currentView)
-                currentView.close();
-            */
-            currentView = view;
-            $content.html(currentView.$el);
-        }
-    }
+          if(currentView)
+              currentView.close();
+          */
+          currentView = view;
+          $content.html(currentView.$el);
+      }
+  }
 
-    return ViewHandler;
+  return ViewHandler;
 });
