f854d827a07b9e3f2cf3d03263cf7d6c4e7e71c5
Jani Nieminen <jpkniem@gmail.com>
2015-08-10 14:00:34 +0300

    Login view styles, and add security(token) handling.

--- src/js/app/router.js
+++ src/js/app/router.js
@@ -1,30 +1,39 @@
 define(function(require) {
 
-	var $ = require('jquery');
- 	var Backbone = require('backbone');
+		var $ = require('jquery');
+ 		var Backbone = require('backbone');
   	var ViewHandler = require('libs/view-handler');
+		var SecurityHandler = require('libs/security');
 
   	var $body = $("body");
   	var $content = $(".content");
 
   	var NavBarView = require('app/navigation/navbar/navbar');
   	var QuizView = require('app/quiz/quiz');
+		var LoginView = require('app/login/login');
   	var StatsView = require('app/stats/stats');
 
   	return Backbone.Router.extend({
 		routes: {
 			'': 'quiz',
+			'login': 'login',
 			'quiz': 'quiz',
 			'stats': 'stats'
 		},
+
 		initialize: function() {
 			this.addNavBar();
-        },
+    },
 
-        addNavBar: function() {
-            var navBarView = new NavBarView();
-            $body.prepend(navBarView.$el);
-        },
+		login: function() {
+			var loginView = new LoginView({el:'.content'});
+			loginView.render();
+		},
+
+    addNavBar: function() {
+      var navBarView = new NavBarView();
+      $body.prepend(navBarView.$el);
+    },
 
 		quiz: function() {
 			var quizView = new QuizView({el:'.content'});
@@ -38,7 +47,6 @@ define(function(require) {
 			var statsView = new StatsView({el:'.content'});
 			Backbone.Transitions.transit(statsView);
 			//ViewHandler.setCurrent(statsView, "Stats");
-
 		}
 	});
 });
